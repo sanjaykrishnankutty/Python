98b962ecb5a6cf133ba3dc79540fb6d2df35228e
Sanjay Krishnankutty <kksanjay@gmail.com>
2025-08-15 22:40:00 +1000

    project chcek in

---
+++ BillCalculator.py
@@ -0,0 +1,9 @@
+def EnableBillCalculator(v1, v2):
+    return v1 + v2
+
+print("Enter first number:")
+v1 = int(input())
+print("Enter second number:")
+v2 = int(input())
+result = EnableBillCalculator(v1, v2)
+print("The result is:", result)
